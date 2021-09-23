e40ec1967dea8cadacfbe548ea9c7bb5122d332a
SadafImaduddin <36033344+SadafImaduddin@users.noreply.github.com>
2021-09-23 21:52:51 +0300

    commit

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "открыть изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(279, 227);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.Location = new System.Drawing.Point(404, 76);
+            this.trackBar1.Maximum = 36;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(178, 45);
+            this.trackBar1.TabIndex = 18;
+            this.trackBar1.TickFrequency = 6;
+            this.trackBar1.Value = 18;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(404, 128);
+            this.trackBar2.Maximum = 20;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(178, 45);
+            this.trackBar2.TabIndex = 19;
+            this.trackBar2.Value = 10;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(404, 179);
+            this.trackBar3.Maximum = 20;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(178, 45);
+            this.trackBar3.TabIndex = 20;
+            this.trackBar3.Value = 10;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(333, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Hue";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(330, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Saturation";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(333, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Value";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(2, 235);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(668, 427);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 665);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bmpHSV;
+        private Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string imageLocation = "";
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(*.jpg)|*.jpg| PNG files|*.png| All files(*.*)|*.*";
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+                    pictureBox1.ImageLocation = imageLocation;
+                    pictureBox2.ImageLocation = imageLocation;
+                    bmp = new Bitmap(pictureBox1.ImageLocation);
+                    bmpHSV = new Bitmap(pictureBox1.ImageLocation);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Невозмоно открыть выбранный файл","Ошибка",
+                    MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+        
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int a = trackBar1.Value;
+            int b = trackBar2.Value;
+            int c = trackBar3.Value;
+            HSV(a, b, c);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            int a = trackBar1.Value;
+            int b = trackBar2.Value;
+            int c = trackBar3.Value;
+            HSV(a, b, c); 
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            int a = trackBar1.Value;
+            int b = trackBar2.Value;
+            int c = trackBar3.Value;
+            HSV(a, b, c); 
+        }
+        /**/
+        private void HSV(int a, int b, int c)
+        {
+            double h, s, v;
+            for (int y = 0; y < bmpHSV.Height; y++)
+                for (int x = 0; x < bmpHSV.Width; x++)
+                {
+                    RGBtoHSV(bmp.GetPixel(x, y), out h, out s, out v);
+                    h = ((int)(h + a * 10 + 180)) % 360;
+                    int s1 = ((int)((s * 100) + b * 10 - 100));
+                    if (s1 > 100)
+                        s = 1;
+                    else if (s1 < 0)
+                        s = 0;
+                    else s = s1 * 0.01;
+
+                    int v1 = ((int)((v * 100) + c * 10 - 100));
+                    if (v1 > 100)
+                        v = 1;
+                    else if (v1 < 0)
+                        v = 0;
+                    else v = v1 * 0.01;
+                    bmpHSV.SetPixel(x, y, HSVtoRGB(h, s, v));
+                }
+            pictureBox2.Image = bmpHSV;
+        }
+        private void RGBtoHSV(Color c, out double h, out double s, out double v)
+        {
+            double r = c.R / (255 * 1.0);
+            double g = c.G / (255 * 1.0);
+            double b = c.B / (255 * 1.0);
+            double min_ = Math.Min(r, Math.Min(g, b));
+            double max_ = Math.Max(r, Math.Max(g, b));
+            if (max_ == min_)
+                h = 0;
+            else if (max_ == r)
+            {
+                h = 60 * (g - b) / (max_ - min_);
+                if (g < b)
+                    h += 360;
+            }
+            else if (max_ == g)
+                h = 60 * (b - r) / (max_ - min_) + 120;
+            else
+                h = 60 * (r - g) / (max_ - min_) + 240;
+
+            s = max_ == 0 ? 0 : 1 - (min_ / max_);
+
+            v = max_;           
+
+        }
+        private Color HSVtoRGB(double h, double s, double v)
+        {
+            int H_i = ((int)Math.Floor(h / 60)) % 6;
+            double f = h / 60 - Math.Floor(h / 60);
+            int p = (int)(v * (1 - s) * 255);
+            int q = (int)(v * (1 - f * s) * 255);
+            int t = (int)(v * (1 - (1 - f) * s) * 255);
+            int V = (int)(v * 255);
+
+            switch (H_i)
+            {
+                case 0: return Color.FromArgb(V, t, p);
+                case 1: return Color.FromArgb(q, V, p);
+                case 2: return Color.FromArgb(p, V, t);
+                case 3: return Color.FromArgb(p, q, V);
+                case 4: return Color.FromArgb(t, p, V);
+                default: return Color.FromArgb(V, p, q);               
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image.Save(pictureBox2.ImageLocation.Replace(".", "(1)."));
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save(pictureBox1.ImageLocation.Replace(".", "(0)."));
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ToolTip t = new ToolTip();
+            t.SetToolTip(pictureBox2, "Чтобы сохранить изображение, нажмите на него");
+
+        }
+    }
+}
